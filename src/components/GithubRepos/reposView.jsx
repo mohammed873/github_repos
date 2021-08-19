c1b7bd8127184628392a67ad2eb2f4538197edea
mohammed elhachimi <mohammed.elhachimi@halsson.com>
2021-08-19 17:05:19 +0100

    added the on scroll  pagination feature , next work on the style

--- src/components/GithubRepos/reposView.jsx
+++ src/components/GithubRepos/reposView.jsx
@@ -1,4 +1,4 @@
-import React , { useState , useEffect } from 'react';
+import React , { useState , useEffect , useRef , useCallback } from 'react';
 import styles from '../../styles/reposView.module.css'
 import GetStarsReposInlast30Days from './reposLogic.jsx'
 
@@ -9,32 +9,78 @@ function ReposView() {
   //call back the custum hook for fetching all stars repos in last 30 days
   const {loading , repos , hasMore} = GetStarsReposInlast30Days(pageNumber)
 
+  //initial useRef
+  const observer = useRef()
+
+  //create a const for the last repo rendred 
+  const lastRepoRef = useCallback( node =>{
+
+    if (loading) return
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver( entries =>{
+        if(entries[0].isIntersecting && hasMore){
+          setPageNumber(previousPageNumber => previousPageNumber + 1)
+        }
+    })
+    if(node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+
+
+
+
+
 
   useEffect(() => {
    setPageNumber(1)
   }, [])
   return (
     <>
-         {repos.map(repo=>{
-           return (
-              <div key={repo.id} className={styles.repoContainer}>
-                 <div className={styles.ownerAvatarContainer}>
-                      <img src={repo.owner.avatar_url} alt="owner avatar" />
+         {repos.map((repo , index ) => {
+            if(repos.length === index + 1) {
+              return (
+                <div ref={lastRepoRef} key={repo.id} className={styles.repoContainer}>
+                   <div className={styles.ownerAvatarContainer}>
+                        <img src={repo.owner.avatar_url} alt="owner avatar" />
+                    </div>
+                    <div className={styles.repoDetailsContainer}>
+                        <h1>{repo.name}</h1>
+                        <p>{repo.description}</p>
+                        <span className={styles.starsSpan}>Stars {repo.stargazers_count}</span>
+                        <span className={styles.issuesSpan}>Issues {repo.open_issues_count}</span>
+                        <span className={styles.submissionDateSpan}> 
+                           created on {repo.created_at} by  
+                           <span className={styles.ownerNameSpan}>
+                             {repo.owner.login}
+                           </span>
+                        </span>
+                    </div>
                   </div>
-                  <div className={styles.repoDetailsContainer}>
-                      <h1>{repo.name}</h1>
-                      <p>{repo.description}</p>
-                      <span className={styles.starsSpan}>Stars {repo.stargazers_count}</span>
-                      <span className={styles.issuesSpan}>Issues {repo.open_issues_count}</span>
-                      <span className={styles.submissionDateSpan}> 
-                         created on {repo.created_at} by 
-                         <span className={styles.ownerName}>
-                           {repo.owner.login}
-                         </span>
-                      </span>
+              )
+            }else{
+              return (
+                <div key={repo.id} className={styles.repoContainer}>
+                   <div className={styles.ownerAvatarContainer}>
+                        <img src={repo.owner.avatar_url} alt="owner avatar" />
+                    </div>
+                    <div className={styles.repoDetailsContainer}>
+                        <h1>{repo.name}</h1>
+                        <p>{repo.description}</p>
+                        <span className={styles.starsSpan}>Stars {repo.stargazers_count}</span>
+                        <span className={styles.issuesSpan}>Issues {repo.open_issues_count}</span>
+                       
+                        <span className={styles.submissionDateSpan}> 
+                           created on {repo.created_at} by  
+                           <span className={styles.ownerNameSpan}>
+                             {repo.owner.login}
+                           </span>
+                        </span>
+                    </div>
                   </div>
-                </div>
-           )})}
+              )
+            }
+           })}
+            <div>{loading && 'Loading ...'}</div>
     </>
   );
 }
